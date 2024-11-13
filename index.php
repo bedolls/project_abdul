90025f59321717e42c0c275e6ee428765aad45d3
bedolls <abdullmaliik.alf@gmail.com>
2024-11-13 15:28:04 +0700

    selamat datang

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>project IS62</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php
+include ('navbar.php');
+?>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+        <div class="card">
+  <div class="card-header text-center">
+   <h3>Selamat Datang</h3> 
+  </div>
+  <div class="card-body text-center">
+    <h2>SISTEM INFORMASI</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem est voluptatibus architecto autem vero voluptate sequi earum voluptas dolor rerum tempore,
+       fuga nesciunt ex quas. Accusamus optio fuga totam repudiandae!</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
