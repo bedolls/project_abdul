1c3bc48f0286580c50cbc6ed42b89b97d4254eb8
bedolls <abdullmaliik.alf@gmail.com>
2024-11-13 15:28:19 +0700

    cut copy disini

---
+++ modul_jurusan/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data jurusan</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+<?php
+include ('../navbar.php');
+?>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+        <div class="card">
+  <div class="card-header text-center">
+   <h3>Sdata jurusan</h3> 
+  </div>
+  <div class="card-body text-center">
+    <h2>hallo,selamat datang</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem est voluptatibus architecto autem vero voluptate sequi earum voluptas dolor rerum tempore,
+       fuga nesciunt ex quas. Accusamus optio fuga totam repudiandae!</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+</body>
+</html>
