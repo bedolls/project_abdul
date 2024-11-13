716b6bcc5641f4c44ff61fbfaf438ae0f3bbd779
bedolls <abdullmaliik.alf@gmail.com>
2024-11-13 16:51:21 +0700

    penyesuain tampilan index jurusan dan tambah koneksi

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "project_abd";
+
+$koneksi = mysqli_connect($host, $username, $password, $database);
+
+
+?>
--- modul_jurusan/index.php
+++ modul_jurusan/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>data jurusan</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/all.css">
 </head>
 <body>
 <?php
@@ -14,13 +15,32 @@ include ('../navbar.php');
     <div class="row mt-5">
         <div class="col-6 m-auto">
         <div class="card">
-  <div class="card-header text-center">
-   <h3>data jurusan</h3> 
+  <div class="card-header ">
+   <h3 class="float-start">Data jurusan</h3> 
+   <span class="float-end" ><a class="btn btn-primary" href=""><i class="fa-solid fa-square-plus"></i> Tambah Data</a><span>
   </div>
-  <div class="card-body text-center">
-    <h2>hallo,selamat datang</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem est voluptatibus architecto autem vero voluptate sequi earum voluptas dolor rerum tempore,
-       fuga nesciunt ex quas. Accusamus optio fuga totam repudiandae!</p>
+  <div class="card-body ">
+  <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Kode</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>
+      <a class="btn btn-success" href=""><i class="fa-solid fa-pen-to-square"></i></a>
+      <a class="btn btn-danger" href=""><i class="fa-solid fa-trash"></i></a>
+      </td>
+    </tr>
+  </tbody>
+</table>
 </div>
 </div>
 </div>
@@ -28,5 +48,6 @@ include ('../navbar.php');
 </div>
     <script src="../js/bootstrap.js"></script>
     <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/all.js"></script>
 </body>
 </html>
